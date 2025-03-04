c445907584349e596ccacc64ac9a2567d68cf651
Hélène AUERBACH <ln_au@yahoo.fr>
2025-03-04 04:07:55 +0100

    Use of textures files pathes extracted from the .cub scene file

--- cub3d/srcs/textures.c
+++ cub3d/srcs/textures.c
@@ -6,7 +6,7 @@
 /*   By: rmorice <rmorice@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/27 14:17:11 by rmorice           #+#    #+#             */
-/*   Updated: 2025/03/02 23:33:54 by rmorice          ###   ########.fr       */
+/*   Updated: 2025/03/04 04:03:54 by hauerbac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,13 +35,8 @@ static t_mlx_img	load_xpm_textures(t_mlx *mlx, char *path)
 
 void	init_textures(t_c3d_data *c3d)
 {
-	char	*no_path = "textures/north.xpm"; //"textures/BRICK_1A.xpm";
-	char	*ea_path = "textures/east.xpm"; //"textures/GRID_2B.xpm";
-	char	*so_path = "textures/south.xpm"; //"textures/PIPES_2B.xpm";
-	char	*we_path = "textures/west.xpm"; //"textures/WOOD_1C.xpm";
-
-	c3d->m_col.tex_no = load_xpm_textures(&c3d->mlx, no_path);
-	c3d->m_col.tex_ea = load_xpm_textures(&c3d->mlx, ea_path);
-	c3d->m_col.tex_so = load_xpm_textures(&c3d->mlx, so_path);
-	c3d->m_col.tex_we = load_xpm_textures(&c3d->mlx, we_path);
+	c3d->m_col.tex_no = load_xpm_textures(&c3d->mlx, c3d->textures.NO);
+	c3d->m_col.tex_ea = load_xpm_textures(&c3d->mlx, c3d->textures.EA);
+	c3d->m_col.tex_so = load_xpm_textures(&c3d->mlx, c3d->textures.SO);
+	c3d->m_col.tex_we = load_xpm_textures(&c3d->mlx, c3d->textures.WE);
 }
